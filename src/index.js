fdeb548c164e4f602e03578d3ed33f3ed9dd6cae
agdsarge <agdsargeant@gmail.com>
2020-06-05 10:50:26 -0400

    challenge 2 complete

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch(imgUrl)
-    .then(resp => resp.json())
-    .then(jAry => jAry["message"].forEach(addImgToDom))
+        .then(resp => resp.json())
+        .then(json => json["message"].forEach(addImgToDom))
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(json => json["message"]) //we now have an object
+        .then(msg => {
+            for (const key in msg) {
+                addBreedToDom(key, msg[key])
+            }
+        })
 })
 
 function addImgToDom(imageURL) {
@@ -14,4 +22,23 @@ function addImgToDom(imageURL) {
     let pic = document.createElement("img")
     pic.src = imageURL
     imageContainer.appendChild(pic)
+}
+
+function addBreedToDom(breedKey, breedAry) {
+    //console.log(breedKey)
+
+    let ulBreed = document.querySelector("#dog-breeds")
+    if (breedAry.length === 0) {
+        let li = document.createElement("li")
+        li.innerText = breedKey
+        ulBreed.appendChild(li)
+    }
+    else {
+        for (let ind = 0; ind < breedAry.length; ind++) {
+            let li = document.createElement("li")
+            li.innerText = `${breedAry[ind]} ${breedKey}`
+            ulBreed.appendChild(li)
+        }    
+    }
+
 }
