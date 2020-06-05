1e0f7ff6a4495f86448b79734eb192536af9597d
Injae Lee <github email address>
2020-06-05 11:02:02 -0400

    challenge1-finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+
+addEventListener("DOMContentLoaded", openFunction) 
+
+function getDogImage (){
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json() )
+    .then(data => {
+        data.message.forEach( pictureURL => {
+            renderImage(pictureURL)
+        }) 
+        console.log(data.message)
+    })  
+}
+
+function renderImage (pickURL) {
+    let imageContainer = document.getElementById("dog-image-container")
+    let imageList = document.createElement("li")
+    imageContainer.appendChild(imageList)
+    let image = document.createElement("img")
+    image.src = pickURL
+    image.alt = "image of dog"
+    image.style.width = "50%"
+    imageList.appendChild(image)
+}
+
+
+
+function openFunction(){
+    getDogImage()
+}
+
