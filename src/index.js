7bd429c9bdb71099e7ace114e4fe8ccd1497793d
agdsarge <agdsargeant@gmail.com>
2020-06-05 10:31:51 -0400

    challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(jAry => jAry["message"].forEach(addImgToDom))
+})
+
+function addImgToDom(imageURL) {
+    let imageContainer = document.querySelector("#dog-image-container")
+    let pic = document.createElement("img")
+    pic.src = imageURL
+    imageContainer.appendChild(pic)
+}
